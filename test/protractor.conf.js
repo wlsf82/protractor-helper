52725bfe37485dcb060e66d739a67671543b2c3c
wlsf82 <wlsf82@gmail.com>
2018-03-02 22:38:48 +0100

    update test config file to run tests on headless browser

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,6 +2,16 @@ const constants = require("./constants");
 
 module.exports.config = {
     baseUrl: constants.BASE_URL,
+    capabilities: {
+        browserName: "chrome",
+        chromeOptions: {
+            args: [
+                "--headless",
+                "--disable-gpu",
+                "--window-size=1024,768"
+            ]
+        }
+    },
     specs: ["*.spec.js"],
     onPrepare() { browser.ignoreSynchronization = true },
     jasmineNodeOpts: {
