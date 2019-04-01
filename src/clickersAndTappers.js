806a731f614963ec7921a21e8862e2d4c1ca2ccf
Walmyr Filho <limaesilva@qelp.com>
2019-04-01 23:33:32 +0200

    Separate clickers and tapper functions into their on file

---
+++ src/clickersAndTappers.js
@@ -0,0 +1,73 @@
+const constants = require("../constants_and_utils/constants");
+const messageBuilder = require("../constants_and_utils/messageBuilder");
+const utils = require("../constants_and_utils/utils");
+const waiters = require("./waiters");
+
+const config = { timeoutInMilliseconds: constants.DEFAULT_TIMEOUT_IN_MS };
+
+const clickWhenClickable = function(
+  htmlElement = utils.requiredParam(clickWhenClickable),
+  timeoutInMilliseconds = config.timeoutInMilliseconds,
+  errorMessage = messageBuilder.getDefaultIsNotClickableMessage(htmlElement)
+) {
+  utils.waitForElementToBeClickable(
+    htmlElement,
+    timeoutInMilliseconds,
+    errorMessage
+  );
+  htmlElement.click();
+};
+
+const click = function(
+  htmlElement = utils.requiredParam(click),
+  timeoutInMilliseconds = config.timeoutInMilliseconds
+) {
+  utils.waitForElementToBeClickable(
+    htmlElement,
+    timeoutInMilliseconds,
+    messageBuilder.getDefaultIsNotClickableMessage(htmlElement)
+  );
+  htmlElement.click();
+};
+
+const tapWhenTappable = function(
+  htmlElement = utils.requiredParam(tapWhenTappable),
+  timeoutInMilliseconds = config.timeoutInMilliseconds,
+  errorMessage = messageBuilder.getDefaultIsNotTappableMessage(htmlElement)
+) {
+  utils.waitForElementToBeClickable(
+    htmlElement,
+    timeoutInMilliseconds,
+    errorMessage
+  );
+  browser
+    .touchActions()
+    .tap(htmlElement)
+    .perform();
+};
+
+const tap = function(
+  htmlElement = utils.requiredParam(tap),
+  timeoutInMilliseconds = config.timeoutInMilliseconds
+) {
+  const errorMessage = messageBuilder.getDefaultIsNotTappableMessage(
+    htmlElement
+  );
+
+  utils.waitForElementToBeClickable(
+    htmlElement,
+    timeoutInMilliseconds,
+    errorMessage
+  );
+  browser
+    .touchActions()
+    .tap(htmlElement)
+    .perform();
+};
+
+module.exports = {
+  clickWhenClickable,
+  click,
+  tapWhenTappable,
+  tap
+};
