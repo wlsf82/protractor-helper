022b208497cb942f274f8664ef3f5df30041f8f0
Walmyr Filho <limaesilva@qelp.com>
2019-07-08 23:05:40 +0200

    Add unit tests for utils to increase code coverage

---
+++ test/spec/utils.spec.js
@@ -0,0 +1,28 @@
+const utils = require("../../src/constants_and_utils/utils");
+
+describe("utils", () => {
+  it("requiredParam() - passing only mandatory argument", () => {
+    const someFunction = function() {};
+    const actualResult = function() {
+      utils.requiredParam(someFunction)
+    };
+    const expectedResult =
+      `Parameter 'htmlElement' is missing at function 'someFunction()'. 
+\Fill the required parameter.`;
+
+    expect(actualResult).toThrow(new Error(expectedResult));
+  });
+
+  it("requiredParam() - passing all argument", () => {
+    const someOtherFunction = function() {};
+    const requiredParameter = "text"
+    const actualResult = function() {
+      utils.requiredParam(someOtherFunction, requiredParameter)
+    };
+    const expectedResult =
+      `Parameter '${requiredParameter}' is missing at function 'someOtherFunction()'. 
+\Fill the required parameter.`;
+
+    expect(actualResult).toThrow(new Error(expectedResult));
+  });
+});
